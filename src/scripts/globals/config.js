dcaad1c1dfa5aca008a75b9287668003c6c8d4fe
Muhamad Faiz Riyadi <muhammadfaizriyadi@gmail.com>
2024-05-15 23:10:08 +0700

    implement workbox as offline caching

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'MovieCatalogue-V1',
 };
 
 export default CONFIG;
